cdfd5b070e28a357c62261a3af6d1a3c56a7fee2
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-22 15:56:47 -0500

    finished implementing win conditions

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
   $(document).bind('keyup', function(keystroke){
+    var current_position_1 = $("#player1_strip > .active");
+    var current_position_2 = $("#player2_strip > .active");
     if(keystroke.keyCode == 81){
       console.log('we pressed q');
-      $("#player1_strip > .active").removeClass('active');}
+      current_position_1.removeClass('active').next().addClass('active');
+    }
     else if(keystroke.keyCode == 80){
-      console.log('we pressed p');}
+      console.log('we pressed p');
+      current_position_2.removeClass('active').next().addClass('active');
+    }
+    if ($('#player1_strip td:last-child').attr('class') == "active"){
+      alert("Player 1 wins");
+      $(document).unbind('keyup');
+    }
+    // if either player has .lastchild active alert that player wins
+    // disable keyup for q & 
+    else if ($('#player2_strip td:last-child').attr('class') == "active"){
+      alert("Player 2 wins");
+      $(document).unbind('keyup');
+    }
   });
 });
-
-
-// $('body').bind('keyup click',
-//                function(e){
-//                    // stuff to do on 'click' and 'keyup' happens in here...
-//                    alert('woo! A : ' + e.type + ' happened!');
-//                });
